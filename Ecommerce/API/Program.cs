e06a2fc1f1ba1ce6a61259b27489f0500c91e0dd
MAURICIO MARTINS DE CASTRO REGNEL <mauricio.regnel@cs.up.edu.br>
2025-08-29 11:33:55 -0300

    API- Eexmplos de Requisições

--- Ecommerce/API/Program.cs
+++ Ecommerce/API/Program.cs
@@ -1,6 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
+//Funcionalidades
+//Requisições
+// - Endereço/URL
+// - Método HTTP
+
+app.MapGet("/", () => "Minha primeira API");
+
+app.MapGet("/funcionalidade", () => "Minha segunda API");
+app.MapPost("/funcionalidade", () => "Funcionalidade com POST");
 
 app.Run();
