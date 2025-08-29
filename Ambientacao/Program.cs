fd8cab5fe0221cf5645a098e2fdf97c250858a96
MAURICIO MARTINS DE CASTRO REGNEL <mauricio.regnel@cs.up.edu.br>
2025-08-29 09:17:44 -0300

    Projeto de Ambientação e lista de Exercícios

---
+++ Ambientacao/Program.cs
@@ -0,0 +1,15 @@
+﻿//Imprimir alguma coisa na tela
+Console.WriteLine("Digite a sua idade:");
+//Ler alguma informação do console
+int idade = Console.ReadLine();
+
+if (idade >= 18)
+{
+
+}
+else
+{
+    
+}
+
+//Como converter uma string em inteiro
