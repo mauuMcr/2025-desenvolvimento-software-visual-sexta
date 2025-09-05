b573330777701a27d0999a7751e17682040c8b74
MAURICIO MARTINS DE CASTRO REGNEL <mauricio.regnel@cs.up.edu.br>
2025-09-05 11:30:43 -0300

    API - Cadastro r Listagem de Produtos em lista

---
+++ Ecommerce/API/Models/Produto.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace API.Models;
+
+//
+public class Produto
+{
+
+    public Produto()
+    {
+        id = Guid.NewGuid().ToString();
+        CriadoEm = DateTime.Now;
+    }
+
+    
+    public string Nome { get; set; }
+    public string id { get; set; }
+    public int quantidade { get; set; }
+    public double preco { get; set; }
+    public DateTime CriadoEm { get; set; }
+
+}
+
+
--- Ecommerce/API/Program.cs
+++ Ecommerce/API/Program.cs
@@ -1,5 +1,41 @@
+using API.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+
+    List<Produto> produtos = new List<Produto>
+    {
+        new Produto
+        {
+             Nome = "Notebook",
+            quantidade = 10,
+            preco = 4500.00
+        },
+        new Produto
+        {
+            Nome = "Mouse",
+            quantidade = 50,
+            preco = 79.90
+        },
+        new Produto
+        {
+            Nome = "Teclado Mecânico",
+            quantidade = 30,
+            preco = 250.00
+        },
+        new Produto
+        {
+            Nome = "Monitor 24\"",
+            quantidade = 15,
+            preco = 899.99
+        },
+        new Produto
+        {
+            Nome = "Cadeira Gamer",
+            quantidade = 5,
+            preco = 1200.00
+        }
+    };
 
 
 //Funcionalidades
@@ -7,9 +43,24 @@ var app = builder.Build();
 // - Endereço/URL
 // - Método HTTP
 
-app.MapGet("/", () => "Minha primeira API");
+app.MapGet("/", () => "API de Produtos");
 
-app.MapGet("/funcionalidade", () => "Minha segunda API");
-app.MapPost("/funcionalidade", () => "Funcionalidade com POST");
+app.MapGet("/api/produto/listar", () =>
+{
+    return produtos;
+});
+
+app.MapPost("/api/produto/cadastrar", (Produto produto) =>
+{
+
+    produtos.Add(produto);
+
+});
 
 app.Run();
+
+
+Produto produto = new Produto();
+produto.Nome = "Bolacha";
+
+Console.WriteLine(produto.Nome);
